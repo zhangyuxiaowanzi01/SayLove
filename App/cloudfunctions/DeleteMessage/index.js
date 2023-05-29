11a769db63e77747b15c8f9619353fb26f7a909a
一张不够花 <112365898+zhangyuxiaowanzi01@users.noreply.github.com>
2023-05-29 12:42:09 +0800

    Update index.js

--- App/cloudfunctions/DeleteMessage/index.js
+++ App/cloudfunctions/DeleteMessage/index.js
@@ -1,7 +1,7 @@
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
-
-cloud.init()
+const cloud = require('wx-server-sdk')  //  sdk就是微信官方的端口号
+//  直接初始化init自动调用执行
+cloud.init()  
 const db = cloud.database()
 const _ = db.command
 
@@ -17,3 +17,4 @@ exports.main = async (event, context) => {
         console.error(e)
     }
 }
+// 通过云函数调用,使用try语句进行捕获,加强代码的健壮性,返回id的值
